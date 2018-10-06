1b15dfc3443ef6a3b3182ddc88072cb5cc26e0ce
Samuel Knight <knight.samuel@outlook.com>
2018-10-06 15:34:42 +0100

    Added code for bots and q-table representation. Added file for simplifying state.  * high_level_q_agent.py -- bot code, calls q-table functions  * q_learner.py -- q-table code, executes value updates and saving/loading table from existing file  * state_representer.py -- will be used to simplify HFO state before passing on to q-table

---
+++ Q_Agent/high_level_q_agent.py
@@ -0,0 +1,73 @@
+from hfo import *
+import argparse
+from .q_learner import QLearner
+
+
+
+# Taken from: high_level_sarsa_agent.py in HFO repo
+def get_reward(s):
+    reward = 0
+
+    if s == GOAL:
+        reward = 1
+
+    elif s == CAPTURED_BY_DEFENSE:
+        reward = -1
+
+    elif s == OUT_OF_BOUNDS:
+        reward = -1
+
+    elif s == OUT_OF_TIME:
+        reward = -1
+
+    return reward
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=6000)
+    parser.add_argument('--numTeammates', type=int, default=1)
+    parser.add_argument('--numOpponents', type=int, default=1)
+    parser.add_argument('--numEpisodes', type=int, default=1)
+    args = parser.parse_args()
+
+    """ States explained as follows:
+    4 states for location in quartile                       -- 4
+    1 boolean state if quartile is near goal                -- 2
+    Goal scoring angle, SMALL or MED or LARGE               -- 3
+    Goal scoring proximity, CLOSE or FAR or OUT             -- 3
+    For each Teammate:
+        1 boolean state if closer to goal than player       -- 2
+        Proximity to opponent, CLOSE or FAR                 -- 2
+        Pass opening angle, SMALL or LARGE or INVALID       -- 3
+        Goal scoring angle, SMALL or LARGE or INVALID       -- 3
+    For each Opponent:
+        Proximity to said opponent, CLOSE or FAR or OUT     -- 3
+         
+
+    OUT proximity refers to outside of the quartile the player
+    is currently in (not a worry).
+    """
+    NUM_STATES = 12 * 10 * args.numTeammates * 3 * args.numOpponents
+
+    # Shoot, Pass or Dribble
+    NUM_ACTIONS = 3
+
+    hfo = HFOEnvironment()
+    hfo.connectToServer(feature_set=HIGH_LEVEL_FEATURE_SET, server_port=args.port)
+
+    q_learner = QLearner(NUM_STATES, NUM_ACTIONS, q_table_out='/logs/first_table.npy')
+
+    for episode in range(0, args.numEpisodes):
+        status = IN_GAME
+        while status == IN_GAME:
+            features = hfo.getState()
+            if int(features[5] != 1):
+                hfo.act(MOVE)
+            else:
+                pass
+            hfo.act(DRIBBLE)
+            status = hfo.step()
+        if status == SERVER_DOWN:
+            hfo.act(QUIT)
+            exit()
---
+++ Q_Agent/q_learner.py
@@ -0,0 +1,41 @@
+import numpy as np
+import random
+
+class QLearner:
+    def __init__(self, num_states=0, num_actions=0, epsilon=0.01, learning_rate=0.1, discount_factor=0.9,  q_table_in=None, q_table_out=None):
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.eps = epsilon
+        self.learn_rate = learning_rate
+        self.discount = discount_factor
+        self.table_out = q_table_out
+        self.old_state = None
+        if q_table_in:
+            try:
+                self.q_table = np.load(q_table_in)
+            except IOError as ioe:
+                print("Error with file: " + q_table_in)
+                print(ioe.strerror)
+                print("Reinitializing Q Table")
+                self.q_table = np.zeros((num_states, num_actions))
+        else:
+            self.q_table = np.zeros((num_states, num_actions))
+
+    def update(self, state, action, reward):
+        if self.old_state:
+            self.q_table[self.old_state, action] = \
+                (1 - self.learn_rate) * self.q_table[self.old_state, action] + \
+                self.learn_rate * (reward + self.discount * np.amax(self.q_table[state]))
+
+        self.old_state = state
+
+    def get_action(self, state):
+        explore = True if random.random() <= self.epsilon else False
+        if explore:
+            return random.randint(0, self.num_actions)
+
+        return np.argmax(self.q_table[state])
+
+    def save(self):
+        np.save(self.table_out, self.q_table)
+
