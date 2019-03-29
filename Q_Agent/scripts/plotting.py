a6af43949b58d855871c877981d8f3795306ec97
Sammy Knight <knight.samuel@outlook.com>
2019-03-29 13:19:41 +0000

    Fixed plotting code to work with new directory format.

--- Q_Agent/scripts/plotting.py
+++ Q_Agent/scripts/plotting.py
@@ -5,13 +5,12 @@ sns.set(style="whitegrid")
 sns.set_context("paper", rc={"xtick.bottom" : True, "ytick.left" : True})
 import numpy as np
 import os
+from os.path import join
 
 
 def plot_test_data():
     save_img_path = os.path.dirname(os.path.abspath(__file__)) + '/../graphs/2v1_20n_5000t_eps01'
     frames_per_goal, goal_percentages, trials = get_data_from_files()
-    print(goal_percentages)
-    print(frames_per_goal)
     num_experiments = len(frames_per_goal)
     x_vals = [trials * x for x in range(1, num_experiments+1)]
 
@@ -33,6 +32,7 @@ def plot_test_data():
 
     plt.tight_layout()
     plt.savefig(save_img_path)
+
 
 def file_sort(file_name):
     number_file_ext = file_name.split('_')[-1]
@@ -57,8 +57,9 @@ def plot_data(path_to_output_dir, save_image_name, num_train_iterations,
               train_trials):
     save_img_path = os.path.dirname(os.path.abspath(__file__)) + '/../graphs/' + save_image_name + '.png'
     lineplot = add_lineplot(path_to_output_dir, '0.10')
-
-    lineplot.legend(title="Epsilon", loc=4)
+    add_lineplot('/home/sammy/Documents/Diss/HFO/example/custom_agents/HFO_Bots/Q_Agent/output/2019-03-08T02:22:58.268312_agents2_opponents1_eps0.1_lr0.1',
+                 '0.20', lineplot)
+    lineplot.legend(title="Starting Epsilon", loc=4)
     lineplot.minorticks_on()
     lineplot.grid(which='minor', linestyle=':')
     x_vals = [train_trials * x for x in range(0, num_train_iterations+1)]
@@ -72,40 +73,39 @@ def plot_data(path_to_output_dir, save_image_name, num_train_iterations,
 
 
 def get_data_from_files(path_to_output_dir):
-    # path_to_output_dir = os.path.join(
+    # path_to_output_dir = join(
     #     os.path.dirname(os.path.abspath(__file__)) + '/../output/',
     #     path_to_output_dir
     # )
     # path_to_output_dir = abs_path_test_dir
-    test_dirs = []
+    trials, num_train_runs = get_train_stats(path_to_output_dir)
 
-    for tdir in os.listdir(path_to_output_dir):
-        if 'test_iter' in tdir:
-            test_dirs.append(tdir)
-    test_dirs.sort(key=file_sort)
-    
+    run_dirs = []
+    for run_dir in os.listdir(path_to_output_dir):
+        run_dirs.append(run_dir)
+    run_dirs.sort(key=file_sort)
+
     goal_percent_lst = []
     frames_lst = []
 
+    for run_dir in run_dirs:
+        test_iterations = []
+        for iteration in os.listdir(join(path_to_output_dir, run_dir)):
+            if 'test_iter' in iteration:
+                test_iterations.append(join(path_to_output_dir, run_dir, iteration))
+        test_iterations.sort(key=file_sort)
 
-    for dir_index in range(len(test_dirs)):
-        test_files = []
-        directory = test_dirs[dir_index]
-        dir_path = os.path.join(path_to_output_dir, directory)
-        for file in os.listdir(dir_path):
-            file_path = os.path.join(dir_path, file)
-            test_files.append(file_path)
-        test_files.sort(key=file_sort)
-        frames_per_goal, goal_percentage = goal_data_from_files(test_files)
+        frames_per_goal, goal_percentage = goal_data_from_files(test_iterations)
         goal_percent_lst.append(goal_percentage)
         frames_lst.append(frames_per_goal)
 
-    trials, num_train_runs = get_train_stats(path_to_output_dir)
-    goals_df = pd.DataFrame(columns=[x for x in range(trials, trials*(num_train_runs + 1), trials)])
-    print(goals_df)
-    populate_goals(goals_df, goal_percent_lst)
+    goals_df = populate_dataframe(goal_percent_lst, trials, num_train_runs)
     goals_df = goals_df.transpose()
-    goals_df.columns=['test_run_' + str(x) for x in range(0, len(test_files))] 
+    print(goals_df)
+
+    frames_df = populate_dataframe(frames_lst, trials, num_train_runs)
+    print(frames_df)
+    # goals_df.columns = ['test_run_' + str(x) for x in range(0, len(test_files))]
     return goals_df
 
 
@@ -121,22 +121,25 @@ def goal_data_from_files(test_files):
         goal_percent = get_goal_percentage(file_lines[14], file_lines[15])
         goal_percentages.append(goal_percent)
     
-    return np.array(frames_per_goal), np.array(goal_percentages)
-    
-def populate_goals(dataframe, goal_percent_lst):
-    for arr_index in range(len(goal_percent_lst)):
-        print(goal_percent_lst[arr_index])
-        dataframe[dataframe.columns[arr_index]] = goal_percent_lst[arr_index]
+    return frames_per_goal, goal_percentages
+
+
+def populate_dataframe(stats_lst, trials, num_train_runs):
+    dataframe = pd.DataFrame(stats_lst, columns=[x for x in range(trials, trials*(num_train_runs + 1), trials)])
+    return dataframe
+
 
 def get_dataframe_columns(num_test_runs):
     columns = ['training_runs']
     for i in range(num_test_runs):
         columns.append('test_run_' + str(i))
     return columns
-        
+
+
 def get_last_value_float(line):
     words = line.strip().split(' ')
     return float(words[-1])
+
 
 def get_goal_percentage(trial_line, goal_line):
     trials = get_last_value_float(trial_line)
@@ -147,24 +150,25 @@ def get_goal_percentage(trial_line, goal_line):
 def get_train_stats(output_dir):
     path_to_train_file = None
     num_train_runs = 0
-    for file in os.listdir(output_dir):
+    for file in os.listdir(join(output_dir, 'run_0')):
         if 'train_iter_' in file:
             num_train_runs += 1
             if not path_to_train_file:
-                path_to_train_file = os.path.join(output_dir, file)
+                path_to_train_file = join(output_dir, 'run_0', file)
     with open(path_to_train_file, 'r') as fp:
         file_lines = fp.readlines()
 
     return int(get_last_value_float(file_lines[14])), num_train_runs
 
 
-# if __name__ == '__main__':
-#     plot_data(
-#         '/home/samdknight/Documents/Edinburgh/4th/Dissertation/HFO/example/custom_agents/HFO_Bots/Q_Agent/output/2v1_20n_50its_eps01_1000test',
-#         'test',
-#         20,
-#         50
-#     )
+if __name__ == '__main__':
+    # get_data_from_files('/home/sammy/Documents/Diss/HFO/example/custom_agents/HFO_Bots/Q_Agent/output/2019-03-06T12:12:22.551909_agents2_opponents1_eps0.1_lr0.05')
+    plot_data(
+        '/home/sammy/Documents/Diss/HFO/example/custom_agents/HFO_Bots/Q_Agent/output/2019-03-06T12:12:22.551909_agents2_opponents1_eps0.1_lr0.05',
+        'test',
+        20,
+        50
+    )
 
 
 
